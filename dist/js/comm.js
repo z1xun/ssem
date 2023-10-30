ee50d5cab8e7980c929698a53ae47b0c0643dafe
z1xun <jsjh5934@naver.com>
2023-10-30 17:19:04 +0900

    링크수정 및 웹접근성 개선

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,4 +1,3 @@
-console.log("Ddd");
 class Accordion {
     constructor(domNode) {
         this.rootEl = domNode;
@@ -8,8 +7,6 @@ class Accordion {
         this.contentEl = document.getElementById(controlsId);
 
         this.open = this.buttonEl.getAttribute("aria-expanded") === "true";
-
-        // add event listeners
         this.buttonEl.addEventListener("click", this.onButtonClick.bind(this));
     }
 
@@ -18,15 +15,12 @@ class Accordion {
     }
 
     toggle(open) {
-        // don't do anything if the open state doesn't change
         if (open === this.open) {
             return;
         }
 
-        // update the internal state
         this.open = open;
 
-        // handle DOM updates
         this.buttonEl.setAttribute("aria-expanded", `${open}`);
         if (open) {
             this.contentEl.removeAttribute("hidden");
@@ -35,7 +29,6 @@ class Accordion {
         }
     }
 
-    // Add public open and close methods for convenience
     open() {
         this.toggle(true);
     }
@@ -45,7 +38,6 @@ class Accordion {
     }
 }
 
-// init accordions
 const accordions = document.querySelectorAll(".accordion h3");
 accordions.forEach((accordionEl) => {
     new Accordion(accordionEl);
